61f89abe1d3dec830ba87fff75467a9006e27baa
Ruben <rubenpm88@gmail.com>
2021-01-18 19:06:27 +0100

    added eyedetection functionality

--- camera.py
+++ camera.py
@@ -20,7 +20,13 @@ class VideoCamera(object):
         gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         face_rects=face_cascade.detectMultiScale(gray,1.3,5)
         for (x,y,w,h) in face_rects:
-        	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-        	break
+            cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = image[y:y+h, x:x+w]
+            eyes = eyeCascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+        	#break
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
--- main.py
+++ main.py
@@ -19,4 +19,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.38',port='5000', debug=True)
+    #app.run(host='192.168.1.10',port='5000', debug=True)
+    app.run(host='0.0.0.0',port='5000', debug=True)
